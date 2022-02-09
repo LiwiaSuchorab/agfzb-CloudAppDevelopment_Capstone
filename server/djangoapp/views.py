5bd135b725c7c5316739fa7a261043e28ad711d3
LiwiaSuchorab <liwia.suchorab@gmail.com>
2022-02-09 12:53:41 +0000

    login

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -36,6 +36,20 @@ def contact(request):
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 # ...
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+         return render(request, 'djangoapp/index.html', context)
+        else:
+            return render(request, 'djangoapp/index.html', context)
+    else:
+        return render(request, 'djangoapp/index.html', context)
+
 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
